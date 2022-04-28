09d15c5c2c1a9571ad96f500eb846d66c7ffa174
AnaPinho1990 <anagonpinho@gmail.com>
2022-04-28 14:47:15 +0100

    add new background, start button and fullscreen

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 let startScreen = document.getElementById("start-screen");
+let startButton = document.getElementById("start-button");
 
 console.log(startScreen);
 
 window.onload = () => {
+  const startMusic = new Audio("./docs/assets/sounds/intro.webm");
+  startMusic.play();
+
   document.getElementById("start-button").onclick = () => {
-    startScreen.style.display = "none"; // posso mudar de backgrounds por ex
+    startScreen.style.display = "none";
+    startButton.style.display = "none"; // posso mudar de backgrounds por ex
     startGame();
   };
 
